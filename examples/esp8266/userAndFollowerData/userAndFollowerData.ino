be2c18affcb9ebe6890227ba97a427d962b04525
bajoraner <66447312+bajoraner@users.noreply.github.com>
2020-06-06 17:34:48 +0200

    Certifikate & Accesstoken

--- examples/esp8266/userAndFollowerData/userAndFollowerData.ino
+++ examples/esp8266/userAndFollowerData/userAndFollowerData.ino
@@ -8,24 +8,39 @@
 #include <TwitchApi.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
-
+#include "CACert.h"
 #include <ArduinoJson.h> // This Sketch doesn't technically need this, but the library does so it must be installed.
 
 //------- Replace the following! ------
-char ssid[] = "SSID";         // your network SSID (name)
+char ssid[] = "ssid";         // your network SSID (name)
 char password[] = "password"; // your network key
 
 // Create a new application on https://dev.twitch.tv/
-#define TWITCH_CLIENT_ID "1234567890654rfsc"
+#define TWITCH_CLIENT_ID "***"
+#define TWITCH_ACCESSTOKEN "***"
 
 // Username of who you are getting the data for (e.g. "ninja")
-#define TWITCH_LOGIN "brianlough"
+#define TWITCH_LOGIN "bajoraner"
 
 WiFiClientSecure client;
-TwitchApi twitch(client, TWITCH_CLIENT_ID);
+TwitchApi twitch(client, TWITCH_CLIENT_ID, TWITCH_ACCESSTOKEN);
 
 unsigned long delayBetweenRequests = 60000; // Time between requests (1 minute)
 unsigned long requestDueTime;               //time when request due
+
+
+
+// Load root certificate in DER format into WiFiClientSecure object
+void setCertificate()
+{
+    bool res = client.setCACert_P(caCert, caCertLen);
+    if (!res) {
+      Serial.println("Failed to load root CA certificate!");
+      while (true) {
+      yield();
+      }
+    }
+}
 
 void setup()
 {
@@ -54,6 +69,9 @@ void setup()
     Serial.println("IP address: ");
     IPAddress ip = WiFi.localIP();
     Serial.println(ip);
+
+        //Set Twitch.tv Root Certificate
+    setCertificate();
 }
 
 void loop()
@@ -71,58 +89,58 @@ void loop()
             Serial.println("---------User Details ---------");
             Serial.print("Id: ");
             Serial.println(user.id);
-    
+
             Serial.print("Login: ");
             Serial.println(user.login);
-    
+
             Serial.print("Display Name: ");
             Serial.println(user.displayName);
-    
+
             Serial.print("Broadcaster Type: ");
             Serial.println(user.broadCasterType);
-    
+
             Serial.print("Description: ");
             Serial.println(user.description);
-    
+
             Serial.print("Profile Image URL: ");
             Serial.println(user.profileImageUrl);
-    
+
             Serial.print("Offline Image URL: ");
             Serial.println(user.offlineImageUrl);
-    
+
             Serial.print("View Count: ");
             Serial.println(user.viewCount);
             Serial.println("------------------------");
-    
+
             // Follower data requires the user Id, this doesn't change
             // so you can hardcode it into the sketch after you get it once.
             // e.g. char *userId = "171235731";
-    
+
             FollowerData followerData = twitch.getFollowerData(user.id);
             if(!followerData.error){
                 Serial.println("---------Follower Data ---------");
-            
+
                 Serial.print("Number of Followers: ");
                 Serial.println(followerData.total);
-        
+
                 Serial.print("Last Follower Id: ");
                 Serial.println(followerData.fromId);
-        
+
                 Serial.print("Last Follower Name: ");
                 Serial.println(followerData.fromName);
-        
+
                 Serial.print("Last Follower to Id: ");
                 Serial.println(followerData.toId);
-        
+
                 Serial.print("Last Follower to Name: ");
                 Serial.println(followerData.toName);
-        
+
                 Serial.print("Last Follower at: ");
                 Serial.println(followerData.followedAt);
-        
+
                 Serial.println("------------------------");
             }
-        }      
+        }
 
         requestDueTime = millis() + delayBetweenRequests;
     }
